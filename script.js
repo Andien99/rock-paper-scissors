b1e221fd401712a9d69a869167a2e402e3054cae
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-02-12 11:27:03 +1100

    wrote general psudocode framework that will be the basis of the game

---
+++ script.js
@@ -0,0 +1,10 @@
+Psudocode
+// 1. User will select a choice between variables: "rock", "paper", "scissors"
+// 2. Prompt CPU to generate a number (generateNumber) between 0 to 1
+// 3. If:
+    // generateNumber < 0.33: assign "scissors"
+    // generateNumber is [0.33,0.66]: assign "rock"
+    // generateNumber > 0.66: assign "paper"
+// 4. Each this number will be inputted in a unique function based on the player's variable choice
+// 5. The function will determine, winner,loser and draw
+
