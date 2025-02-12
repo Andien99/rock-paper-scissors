f5595b590dc5e23a7535d672c6a56acd2ee85408
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-02-12 16:14:57 +1100

    fixed a bug that caused CPU to generate a choice twice

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Psudocode
-// 1. User will select a choice between variables: "rock", "paper", "scissors"
-// 2. Prompt CPU to generate a number (generateNumber) between 0 to 1
+// 1. Promt User will select a choice between variables: "rock", "paper", "scissors"
+// 2. Ask CPU to generate a number (generateNumber) between 0 to 1
 // 3. If:
     // generateNumber < 0.33: assign "scissors"
-    // generateNumber is [0.33,0.66]: assign "rock"
     // generateNumber > 0.66: assign "paper"
+    // generateNumber else: assign "rock"
+    
 // 4. Each this number will be inputted in a unique function based on the player's variable choice
 // 5. The function will determine, winner,loser and draw
 
@@ -18,7 +19,7 @@ function getComputerChoice() {
         return "rock"; 
     }}
 let computerChoice = getComputerChoice();
-console.log ("the computer played " + getComputerChoice())
+console.log ("the computer played " + computerChoice)
 
 let humanChoice = prompt("rock,paper or scissors?")
 
@@ -29,16 +30,16 @@ function playRound() {
 if (computerChoice == humanChoice) {
     return "same"
 } else if (computerChoice == "paper" && humanChoice == "rock") {
-    return "you lose 1!";
+    return "you lose!";
 } else if (computerChoice == "scissors" && humanChoice == "rock") {
-    return "you win 1!";
+    return "you win!";
 } else if (computerChoice == "scissors" && humanChoice == "paper") {
-    return "you lose 2!";
+    return "you lose!";
 } else if (computerChoice == "rock" && humanChoice == "paper") {
-    return "you win 2!";
+    return "you win!";
 } else if (computerChoice == "rock" && humanChoice == "scissors") {
-    return "you lose 3!";
+    return "you lose!";
 } else (computerChoice == "paper" && humanChoice == "scissors") 
-    return "you win 3!";
+    return "you win!";
 }
 console.log(playRound())
