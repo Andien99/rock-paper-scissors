cd6f939068d7324ce32594cb605ff59b5ae11bb6
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-02-12 11:43:10 +1100

    added function to getComputerChoice and assigned it to rock,paper or scissors

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Psudocode
+// Psudocode
 // 1. User will select a choice between variables: "rock", "paper", "scissors"
 // 2. Prompt CPU to generate a number (generateNumber) between 0 to 1
 // 3. If:
@@ -8,3 +8,12 @@ Psudocode
 // 4. Each this number will be inputted in a unique function based on the player's variable choice
 // 5. The function will determine, winner,loser and draw
 
+function getComputerChoice() {
+    if (Math.random() > 0.66) {
+        return "scissors";
+    } else if ((Math.random() < 0.33)) {
+        return "paper";
+    } else {
+        return "rock"; 
+    }}
+console.log(getComputerChoice())
