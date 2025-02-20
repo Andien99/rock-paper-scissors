8d61f635a9f771a35b2fc4796815c2602e6214be
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-02-20 13:54:45 +1100

    added basic UI framework and scoreboard to display

--- script.js
+++ script.js
@@ -9,71 +9,106 @@
 // 4. Each this number will be inputted in a unique function based on the player's variable choice
 // 5. The function will determine, winner,loser and draw
 
-// function getComputerChoice() {
-//     if (Math.random() > 0.66) {
-//         return "scissors";
-//         let 
-//     } else if ((Math.random() < 0.33)) {
-//         return "paper";choice
-//     } else {
-//         return "rock"; 
-//     }}
-// let computerChoice = getComputerChoice();
-// console.log ("the computer played " + computerChoice)
 
+let humanScore = 0;
+let computerScore = 0;
+const scoreBoard = document.querySelector("#scoreBoard")
+let displayHumanScore = document.createElement("p");
+    displayHumanScore.textContent = "Player 1: " + humanScore
+    scoreBoard.appendChild(displayHumanScore);
+let displayComputerScore = document.createElement("p");
+    displayComputerScore.textContent = "CPU: " + computerScore
+    scoreBoard.appendChild(displayComputerScore);
+// let computerChoice = getComputerChoice();
+// console.log ("the computer played " + computerChoice)  
 // let humanChoice = prompt("rock,paper or scissors?").toLowerCase()
 
+let computerChoice = "";
+getComputerChoice()
 
-// console.log ("you played " + humanChoice)
-let humanScore = 0;
-let computerScore =0;
-for (var i = 0; i <= 5; i++) {
-function playGame() {
-    function getComputerChoice() {
-        if (Math.random() > 0.66) {
-            return "scissors";
-        } else if ((Math.random() < 0.33)) {
-            return "paper";
-        } else {
-            return "rock"; 
-        }}
-    let computerChoice = getComputerChoice();
-    console.log ("the computer played " + computerChoice)
-    
-    let humanChoice = prompt("rock,paper or scissors?").toLowerCase()
-    
-    
-    console.log ("you played " + humanChoice)
-    function playRound() {
+function getComputerChoice() {
+    if (Math.random() > 0.66) {
+        computerChoice = "scissors"
+        return "scissors";
+    } else if ((Math.random() < 0.33)) {
+        computerChoice = "paper"
+        return "paper";
+    } else {
+        computerChoice = "rock"
+        return "rock"; 
+    }}
+
+
+//DOM Manipulation Section
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log("Player chose " + button.id);
+        let humanChoice = button.id
+        console.log("CPU played " + computerChoice)
+        playRound(button.id, computerChoice);
+        getComputerChoice()
+    })
+});
+
+    // console.log ("you played " + humanChoice)
+    function playRound(humanChoice, computerChoice) {
         if (computerChoice == humanChoice) {
             console.log("The score is " + humanScore + " to " + computerScore);
+            displayHumanScore.textContent = "Player 1: " + humanScore
+            displayComputerScore.textContent = "CPU: " + computerScore
+            checkForWinner()
             return "It's a tie!"
         } else if (computerChoice == "paper" && humanChoice == "rock") {
             ++computerScore;
             console.log("The score is " + humanScore + " to " + computerScore);
+            displayHumanScore.textContent = "Player 1: " + humanScore
+            displayComputerScore.textContent = "CPU: " + computerScore
+            checkForWinner()
             return "you lose!";
         } else if (computerChoice == "scissors" && humanChoice == "rock") {
             ++humanScore;
             console.log("The score is " + humanScore + " to " + computerScore);
+            displayHumanScore.textContent = "Player 1: " + humanScore
+            displayComputerScore.textContent = "CPU: " + computerScore
+            checkForWinner()
             return "you win!";
         } else if (computerChoice == "scissors" && humanChoice == "paper") {
             ++computerScore;
             console.log("The score is " + humanScore + " to " + computerScore);
+            displayHumanScore.textContent = "Player 1: " + humanScore
+            displayComputerScore.textContent = "CPU: " + computerScore
+            checkForWinner()
             return "you lose!";
         } else if (computerChoice == "rock" && humanChoice == "paper") {
             ++humanScore;
             console.log("The score is " + humanScore + " to " + computerScore);
+            displayHumanScore.textContent = "Player 1: " + humanScore
+            displayComputerScore.textContent = "CPU: " + computerScore
+            checkForWinner()
             return "you win!";
         } else if (computerChoice == "rock" && humanChoice == "scissors") {
             ++computerScore;
             console.log("The score is " + humanScore + " to " + computerScore);
+            displayHumanScore.textContent = "Player 1: " + humanScore
+            displayComputerScore.textContent = "CPU: " + computerScore
+            checkForWinner()
             return "you lose!";
         } else (computerChoice == "paper" && humanChoice == "scissors") 
             ++humanScore;   
             console.log("The score is " + humanScore + " to " + computerScore);
+            displayHumanScore.textContent = "Player 1: " + humanScore
+            displayComputerScore.textContent = "CPU: " + computerScore
+            checkForWinner()
             return "you win!";
         }
-return playRound();
-}
-console.log(playGame());
-}
+    
+    function checkForWinner() {
+        if (humanScore >= 5) {
+            alert("HUMANS WIN")
+        } else if (computerScore >= 5) {
+            alert("CPU WINS")
+        } else {
+            getComputerChoice()
+        }}
